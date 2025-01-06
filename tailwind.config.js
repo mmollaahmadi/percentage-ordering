13b221a0602f0e6c21e06d86bf5ef20dfd6005ef
mohammad <mollaahmadimohammad@gmail.com>
2025-01-06 19:59:49 +0330

    style(font): add a Persian font
    add the Yekan font into the project.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Yekan: ['Yekan', 'sans-serif'], // Add IranSans to the font family
+      },
+    },
   },
   plugins: [],
 }
