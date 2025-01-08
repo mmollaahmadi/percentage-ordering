e0e826e166777199526a64625ad58c0be4992864
mohammad <mollaahmadimohammad@gmail.com>
2025-01-08 11:36:26 +0330

    feat(general): calculation box
    add a box into active tabs to get entered percentage and then calculate some values.

--- src/components/screen/active-tab-context/ActiveTabContext.jsx
+++ src/components/screen/active-tab-context/ActiveTabContext.jsx
@@ -5,12 +5,32 @@ import Loading from "../../atomic/loading";
 import ActiveItem from "../../composite/active-item";
 import Decimal from "decimal.js";
 import TotalItem from "../../composite/total-item";
+import CustomInput from "../../atomic/input";
+import CustomButton from "../../atomic/button/CustomButton.jsx";
 
-const ActiveTabContext = ({ type }) => {
+const ActiveTabContext = ({type}) => {
     const [loading, setLoading] = React.useState(false);
     const {marketId} = useParams();
     const [data, setData] = React.useState(null);
     const [totalResult, setTotalResult] = React.useState(null);
+    const [percentageValue, setPercentageValue] = React.useState(0);
+    const [calculatedValue, setCalculatedValue] = React.useState([
+        {
+            code: 'remain',
+            title: 'مجموع حجم ارز قابل دریافت',
+            value: null,
+        },
+        {
+            code: 'price',
+            title: 'میانگین قیمت ارز',
+            value: null,
+        },
+        {
+            code: 'total',
+            title: 'مجموع مبلغ قابل پرداخت',
+            value: null,
+        }
+    ]);
 
     const getData = () => {
         try {
@@ -49,21 +69,67 @@ const ActiveTabContext = ({ type }) => {
             }, {value: 0, remain: 0, price: 0});
 
             setTotalResult([
-                {title: 'مجموع باقیمانده', value: sum?.remain},
-                {title: 'میانگین وزنی قیمت', value: new Decimal(sum.price).dividedBy(sum.value).toFixed(3).toString()},
-                {title: 'مجموع ارزش', value: sum?.value},
+                {code: 'remain', title: 'مجموع باقیمانده', value: sum?.remain},
+                {code: 'price', title: 'میانگین وزنی قیمت', value: new Decimal(sum.price).dividedBy(sum.value).toFixed(3).toString()},
+                {code: 'total', title: 'مجموع ارزش', value: sum?.value},
             ]);
+
+            // if percentage is entered then run calculate method too
+            updateCalculatedValues();
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    const updateCalculatedValues = () =>{
+        try {
+            const temp = calculatedValue?.map((item) => ({
+                ...item,
+                value: percentageValue * totalResult?.filter(tr => tr.code === item.code)[0].value / 100,
+            }))
+            setCalculatedValue(temp)
         } catch (e) {
             console.error(e);
         }
     }
 
     useEffect(() => {
-       calculateSummations();
+        calculateSummations();
     }, [data]);
 
-    return loading ? <Loading /> : (
+    return loading ? <Loading/> : (
         <div className={'flex flex-col w-full'}>
+            <div className={'grid grid-cols-8 gap-2 bg-gray-800 w-full p-3 items-end rounded-lg'}>
+                <div className={'col-span-2 flex gap-2 items-end'}>
+                    <CustomInput
+                        label={'درصد ورودی'}
+                        value={percentageValue}
+                        onChange={setPercentageValue}
+                        min={0}
+                        max={100}
+                        type="number"
+                    />
+                    <CustomButton
+                        onClick={() => {
+                            updateCalculatedValues()
+                        }}
+                        label={'محاسبه'}
+                        disabled={!percentageValue || percentageValue === 0}
+                    />
+                </div>
+
+                {calculatedValue?.map((item, index) => <div key={index} className={'col-span-2 flex gap-2'}>
+                    <p>{item?.title}:</p>
+                    <p>
+                        {item?.value ?? '--'}
+                    </p>
+                </div>)}
+            </div>
+            <TotalItem
+                data={totalResult}
+            />
+            <div className={'border-t mt-3'}></div>
+
             {
                 data?.map((item, index) => (
                     <ActiveItem data={item} key={index}/>
