877227cb2713f41f25086b47c975fb14bd72e59c
mohammad <mollaahmadimohammad@gmail.com>
2025-01-08 09:32:41 +0330

    feat(general): BitPin logo
    add the BitPin's logo into the project and use it in header and change the favicon too.

--- src/components/composite/header/Header.jsx
+++ src/components/composite/header/Header.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import BitPinLogo from "../../../assets/images/logo.png";
 
 const Header = () => {
     return (
         <div className={'flex justify-between bg-black py-4 px-6 border-b border-b-gray-700'}>
-            <p className={'text-2xl leading-none'}>
-                بیت‌پین
-            </p>
+            <div className={'flex justify-between gap-3 items-center'}>
+                <img src={BitPinLogo} alt="BitPin Logo" className="mr-4 w-12 object-contain" />
+                <p className={'text-2xl leading-none'}>
+                    بیت‌پین
+                </p>
+            </div>
+
             <p className={'text'}>
                 سفارش‌گذاری درصدی
             </p>
