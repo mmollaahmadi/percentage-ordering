8f797f6a0d8746dfccaac72f18dbe36415214e22
mohammad <mollaahmadimohammad@gmail.com>
2025-01-08 08:40:53 +0330

    feat(market details): back button
    add a button into the title of page section to back to the markets page.

--- src/pages/market-details/MarketDetails.jsx
+++ src/pages/market-details/MarketDetails.jsx
@@ -1,14 +1,24 @@
 import React from "react";
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import Tabs from "../../components/composite/tabs/index.js";
 import ActiveTabContext from "../../components/screen/active-tab-context/index.js";
 import MatchTabContext from "../../components/screen/match-tab-context/index.js";
 
 const MarketDetails = () => {
     const {marketId} = useParams();
+    const navigate = useNavigate();
+
     return (
         <div className={'flex flex-col gap-3'}>
-            <div className={'flex flex-col'}>{`سفارشات و معاملات ${marketId}`}</div>
+            <div className={'flex items-center justify-center gap-10'}>
+                <p>{`سفارشات و معاملات ${marketId}`}</p>
+                <button
+                    className={'w-fit px-3 '}
+                    onClick={() => navigate(-1)}
+                >
+                    بازگشت
+                </button>
+            </div>
             <Tabs
                 tabsData={[
                     {
